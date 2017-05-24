0cb3f9c8101de0807642997abd6fd177b2e1f63c
snagasawa <whitecrane8920@gmail.com>
2017-05-24 18:25:07 +0900

    Rewrite index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var http = require('http');
+const http = require('http')
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+http.createServer((request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"})
+  response.write("Hello World")
+  response.end()
+}).listen(8888)
